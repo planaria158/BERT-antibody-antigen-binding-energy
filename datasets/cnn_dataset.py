e4d3034b98d00293e8cd1b9b583d80c0085d8b1b
mark@arguslab.com <mark@getplunk.com>
2024-05-01 10:13:38 -0700

    slide deck adding code for CNN training

---
+++ datasets/cnn_dataset.py
@@ -0,0 +1,45 @@
+import math
+import torch
+from torch.utils.data import Dataset
+import pandas as pd
+import numpy as np
+from datasets.scFv_dataset import scFv_Dataset
+
+#--------------------------------------------------------
+# Simple wrapper Dataset to turn output from the scFv dataset
+# into a B&W image for use in a CNN model
+#--------------------------------------------------------
+class CNN_Dataset(Dataset):
+    """
+    Emits 2D B&W images and binding energies
+    """
+    def __init__(self, config, csv_file_path, skiprows=0, inference=False):  
+        super().__init__()
+        self.scFv_dataset = scFv_Dataset(config, csv_file_path, skiprows, inference)
+        self.config = config
+        
+    def get_vocab_size(self):
+        return self.scFv_dataset.vocab_size
+
+    def get_block_size(self):
+        return self.config['block_size']
+
+    def __len__(self):
+        return self.scFv_dataset.__len__()
+
+    def _bin(self, x):
+        return format(x, '08b')
+
+    def _make_img(self, x, shape=(46,46)):
+        d = ''.join([self._bin(x[i]) for i in x.numpy()])
+        # turn d into a list of integers, one for each bit
+        d = [int(x) for x in d]    
+        t = torch.tensor(d[:(46*46)], dtype=torch.float32) # this is for 46,46 matrix
+        t = t.reshape(shape)
+        return t
+
+    """ Returns image, kd pairs used for CNN training """
+    def __getitem__(self, idx):
+        dix, kd = self.scFv_dataset.__getitem__(idx)
+        img = self._make_img(dix)
+        return img, kd
